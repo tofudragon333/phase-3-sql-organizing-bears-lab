7805595afccc5e718834410c1bf13bb91afb2ea9
Ashley Wong <tofudragon333@gmail.com>
2022-07-04 01:10:04 -0400

    commit tree 483b2991f7379bfc32194bc152fe90ef711e485b

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 7, "M", "Brown", "Kind", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "F", "Black", "Sassy", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "M", "Polar", "Timid", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 12, "F", "Smol", "Bold", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "M", "Brown", "Adamant", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 8, "M", "Black", "Quiet", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "Polar", "Bold", True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 1, "F", "Polar", "Sad", True);
